ef1e4838fc307b893ae3c68a1d3ccd2171009b86
usamaAshrafPhambra <usamafsd78@gmail.com>
2021-03-21 23:11:31 +0500

    commit three

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
 const express = require("express");
-
 const connectDB = require("./config/db");
-
-const path = require('path');
-
+const path = require("path");
 const App = express();
-
 const cors = require("cors");
-
 connectDB();
-
 App.use(express.json({ extended: false }));
-
 App.use(cors());
-
-
-
 App.use("/", require("./routes/api/task"));
+//serve static assets
+if (process.env.NODE_ENV === "production") {
+  //   //set static folder
+  App.use(express.static("client/build"));
 
-//serve static assets 
-if (process.env.NODE_ENV === 'production' ) {
-//   //set static folder
-  App.use(express.static('client/build')); 
-
-  App.get('*' , (req , res ) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  App.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
-
 const PORT = process.env.PORT || 6060;
-
 App.listen(PORT, () => {
   console.log(`server start on ${PORT}`);
 });
